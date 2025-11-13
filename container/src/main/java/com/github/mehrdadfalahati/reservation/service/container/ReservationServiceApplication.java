a366e0e66e53c6688ed6cc97f57e47fdc822dfcf
MehrdadFalahati <m.falahati2022@gmail.com>
2025-11-13 15:02:44 +0330

    feat: add spring dependency and security layer in the project.

--- container/src/main/java/com/github/mehrdadfalahati/reservation/service/container/ReservationServiceApplication.java
+++ container/src/main/java/com/github/mehrdadfalahati/reservation/service/container/ReservationServiceApplication.java
@@ -1,4 +1,16 @@
 package com.github.mehrdadfalahati.reservation.service.container;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages = "com.github.mehrdadfalahati.reservation.service.dataacces")
+@EnableJpaRepositories(basePackages = "com.github.mehrdadfalahati.reservation.service.dataacces")
+@SpringBootApplication(scanBasePackages = "com.github.mehrdadfalahati.reservation.service")
 public class ReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServiceApplication.class, args);
+    }
 }
